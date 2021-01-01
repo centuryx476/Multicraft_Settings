a6ae79d208c401e1f44c7e014a7be683742a9362
centuryx476 <SNoorizadeh@SandWP.com>
2021-01-01 11:01:42 -0500

    Master_Branch_Commit_02

--- Scripts/Multicraft_Settings.sh
+++ Scripts/Multicraft_Settings.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set TimeZone
+timedatectl set-timezone America/New_York
+
+# Disable Automatic Date/Time
+timedatectl set-ntp 0
+
 # Install Zip
 apt-get install Zip
 
@@ -11,7 +17,6 @@ systemctl disable rsyslog.service
 
 # Apply Updates
 apt-get update && sudo apt-get upgrade
-
 
 
 
